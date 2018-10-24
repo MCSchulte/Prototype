a17886584c93a5ac501661e6b3969504a8389a99
MCSchulte <mcs228lady@gmail.com>
2018-10-23 20:55:06 -0700

    Creating Wall Behavior
    Set up first script --> Still wip

---
+++ Prototype Schulte/Assets/Scripts/Wall Behavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StarCollision(Collider col)
+    {
+        if (col.CompareTag("Star"))
+        {
+            Debug.Log("Star detected");
+        }
+    }
+
+    public void AsteroidCollision(Collider col)
+    {
+        if (col.CompareTag("Asteroid"))
+        {
+            Debug.Log("Asteroid detected");
+        }
+    }
+}

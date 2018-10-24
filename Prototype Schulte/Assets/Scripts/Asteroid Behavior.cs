4f9072c85d3cff80eb49bb353aa5b68402146e8e
MCSchulte <mcs228lady@gmail.com>
2018-10-24 16:45:54 -0700

    Let there be light!
    Made glowing textures, made light for sun, starting on ceiling stars, outlining scripts for objects

---
+++ Prototype Schulte/Assets/Scripts/Asteroid Behavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RoomCollision(Collider col)
+    {
+        if (col.CompareTag("Wall") || col.CompareTag("Floor") || col.CompareTag("Ceiling"))
+        {
+            Debug.Log("Room detected");
+            //It gets deleted
+        }
+    }
+}
---
+++ Prototype Schulte/Assets/Scripts/Star Behavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RoomCollision(Collider col)
+    {
+        if (col.CompareTag("Wall"))
+        {
+            Debug.Log("Wall detected");
+            //It freezes position against the wall
+        }
+        else if (col.CompareTag("Floor"))
+        {
+            Debug.Log("Floor detected");
+            //It gets deleted
+        }
+        else if (col.CompareTag("Ceiling"))
+        {
+            Debug.Log("Ceiling detected");
+            //It freezes position on the ceiling
+        }
+    }
+
+}

63697ce479aa7d90f4526c7cb6eb8f67c4e18aed
MCSchulte <mcs228lady@gmail.com>
2018-10-24 20:21:10 -0700

    Stuff is complicated
    Got the ceiling lights to work and move, can't get player controller to work well but it at least works, still trying to figure out how to spawn things

---
+++ Prototype Schulte/Assets/Scripts/Planet Behavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void rotate()
+    {
+        //idk somehow link the rotation of this planet to the rotation of the model planet
+    }
+}

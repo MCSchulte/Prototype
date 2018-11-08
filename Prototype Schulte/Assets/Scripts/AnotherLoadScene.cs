b9e0318de52a353dd1286c4131bf467e6b2d9117
MCSchulte <mcs228lady@gmail.com>
2018-11-08 09:58:30 -0800

    Updated Oculus integration
    updated it so it would stop giving me errors, adding a new loading scene, made it look pretty

---
+++ Prototype Schulte/Assets/Scripts/AnotherLoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnotherLoadScene : MonoBehaviour {
+
+    public void ChangeScene()
+    {
+        //Changes to Game Scene
+        Debug.Log("Change Scene");
+        SceneManager.LoadScene("Prototype Setup");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}

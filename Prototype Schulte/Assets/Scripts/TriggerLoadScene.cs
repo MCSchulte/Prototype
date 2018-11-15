e0aa93b9adf3fae477195fd8ce1a499c6a12e09f
MCSchulte <mcs228lady@gmail.com>
2018-11-15 11:25:13 -0800

    Attempted to make a button out of a box and collider
    Didn't work because nothing works :))))))

---
+++ Prototype Schulte/Assets/Scripts/TriggerLoadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TriggerLoadScene : MonoBehaviour {
+
+	public void onTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            print("Change Scene");
+            SceneManager.LoadScene("Prototype Setup");
+
+        }
+    }
+}

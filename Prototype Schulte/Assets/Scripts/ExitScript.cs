34209cb7a64906f5c516de58ce78e0730c324480
MCSchulte <mcs228lady@gmail.com>
2018-11-29 20:46:56 -0800

    FINISHED
    You can now reset the scene, roll credits and quit out of the application, there are floating intructions when you get into the space room, THINGS WORK!!!!

---
+++ Prototype Schulte/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour {
+    public Animator anim;
+    public bool xButtonPress = false;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (OVRInput.Get(OVRInput.Button.Three) == true)
+        {
+            xButtonPress = true;
+            if (OVRInput.Get(OVRInput.Button.Four) == true && xButtonPress == true)
+            {
+                anim.Play("Roll_Credits");
+                
+            }
+        }
+        xButtonPress = false;
+    }
+    
+    public void exitPlease()
+    {
+        Application.Quit();
+    }
+}
---
+++ Prototype Schulte/Assets/Scripts/Reset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour {
+
+    public bool xButtonPress = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Button.Three) == true)
+        {
+            xButtonPress = true;
+        } else if (OVRInput.Get(OVRInput.Button.Four) == true && xButtonPress == false)
+        {
+            resetScene();
+        }
+        xButtonPress = false;
+
+    }
+
+    public void resetScene()
+    {
+        SceneManager.LoadScene("Space Room");
+    }
+}
